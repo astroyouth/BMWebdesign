648282d5498e42ba2da93c0d37db3c8242b1150b
astroyouth <alex.dale78@gmail.com>
2020-06-27 20:29:06 +0100

    added start of video.js

--- video.js
+++ video.js
@@ -1,49 +1,43 @@
 // Declare Vars & Read Files
-var fs = require("fs"),
-    http = require("http"),
-    url = require("url"),
-    path = require("path");
+
+var fs = require('fs'),
+    http = require('http'),
+    url = require('url'),
+    path = require('path');
+var movie_webm, movie_mp4, movie_ogg;
+// ... [snip] ... (Read index page)
+fs.readFile(path.resolve(__dirname,"logo7.mp4"), function (err, data) {
+    if (err) {
+        throw err;
+    }
+    movie_mp4 = data;
+});
+// ... [snip] ... (Read two other formats for the video)
+
+// Serve & Stream Video
 
 http.createServer(function (req, res) {
-  if (req.url != "/logo7.mp4") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end('<video src="http://localhost:8888/logo7.mp4" controls></video>');
-  } else {
-    var file = path.resolve(__dirname,"logo7.mp4");
-    fs.stat(file, function(err, stats) {
-      if (err) {
-        if (err.code === 'ENOENT') {
-          // 404 Error if file not found
-          return res.sendStatus(404);
-        }
-      res.end(err);
-      }
-      var range = req.headers.range;
-      if (!range) {
-       // 416 Wrong range
-       return res.sendStatus(416);
-      }
-      var positions = range.replace(/bytes=/, "").split("-");
-      var start = parseInt(positions[0], 10);
-      var total = stats.size;
-      var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-      var chunksize = (end - start) + 1;
-
-      res.writeHead(206, {
-        "Content-Range": "bytes " + start + "-" + end + "/" + total,
-        "Accept-Ranges": "bytes",
-        "Content-Length": chunksize,
-        "Content-Type": "video/mp4"
-      });
-
-      var stream = fs.createReadStream(file, { start: start, end: end })
-        .on("open", function() {
-          stream.pipe(res);
-        }).on("error", function(err) {
-          res.end(err);
+    // ... [snip] ... (Serve client files)
+    var total;
+    if (reqResource == "/logo7.mp4") {
+        total = movie_mp4.length;
+    }
+    // ... [snip] ... handle two other formats for the video
+    var range = req.headers.range;
+    var positions = range.replace(/bytes=/, "").split("-");
+    var start = parseInt(positions[0], 10);
+    var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+    var chunksize = (end - start) + 1;
+    if (reqResource == "/logo7.mp4") {
+        res.writeHead(206, {
+            "Content-Range": "bytes " + start + "-" + end + "/" + total,
+                "Accept-Ranges": "bytes",
+                "Content-Length": chunksize,
+                "Content-Type": "video/mp4"
         });
-    });
-  } 
+        res.end(movie_mp4.slice(start, end + 1), "binary");
+    }
+    // ... [snip] ... handle two other formats for the video
 }).listen(8888, function(){
-	console.log("video server running");
+  console.log("Video Server Is Running")
 });
